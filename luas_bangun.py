45a0fb645bfd7241520599b6995f335ff0f2d338
AzifZR <azifrizky25@gmail.com>
2023-07-02 15:07:27 +0700

    menambahkan coding sama text

---
+++ luas_bangun.py
@@ -0,0 +1,8 @@
+def luas_segitiga() :
+    a = int(input("masukkan jumlah alas segitiga: "))
+    t = int(input("masukkan jumlah tinggi segitiga: "))
+    luas = a*t/2
+    print("Luas segitiga adalah: ", luas)
+
+luas_segitiga()
+
